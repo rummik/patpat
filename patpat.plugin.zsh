79c6bc5d0ee29a8e55b438df266cbaa22da9d3fd
rummik <r@9k1.us>
2014-05-04 04:36:02 -0400

    Something fancy

---
+++ patpat.plugin.zsh
@@ -0,0 +1,46 @@
+#!/bin/zsh
+
+function -pat-su {
+	if [[ $USER != 'root' ]]; then
+		sudo -- $@
+	else
+		$@
+	fi
+}
+
+function pat {
+	if [[ $# -gt 0 ]]; then
+		cmd=$1
+		shift
+	fi
+
+	if functions + "pat-$cmd" > /dev/null; then
+		"pat-$cmd" $@
+		return $?
+	elif [[ $# -gt 0 ]]; then
+		-pat-su $@
+	else
+		print 'Nothing to do.'
+	fi
+}
+
+function pat-install {
+	if [[ -f $1 ]]; then
+		-pat-su dpkg -i $1
+	else
+		-pat-su aptitude $@
+	fi
+}
+
+function pat-update {
+	-pat-su aptitude update
+	-pat-su apt-file update
+}
+
+function pat-find {
+	pat file find $@
+}
+
+function pat-file {
+	-pat-su apt-file $@
+}
